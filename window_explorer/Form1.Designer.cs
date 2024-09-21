c9a3b97ac1acb5567f28625de08fd00abc509c81
nguye <nguye@SIUNHANHONG>
2024-09-21 20:06:34 +0700

    Add project files.

---
+++ window_explorer/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace window_explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picture.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(212, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(588, 279);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 298);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(533, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(576, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Location = new System.Drawing.Point(657, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Zoom in";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(738, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Zoom out";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // picture
+            // 
+            this.picture.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.picture.Controls.Add(this.listBox1);
+            this.picture.Location = new System.Drawing.Point(0, 12);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(200, 274);
+            this.picture.TabIndex = 5;
+            this.picture.TabStop = false;
+            this.picture.Text = "picture";
+            this.picture.Enter += new System.EventHandler(this.picture_Enter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(183, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 332);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "window explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.picture.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox picture;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ window_explorer/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace window_explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string path;
+        string[] imageFiles;
+        bool be_opened=false;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    path = fbd.SelectedPath;
+                    textBox1.Text = path;
+                    imageFiles =null;
+                    if(be_opened)
+                    {
+                        listBox1.Items.Clear();
+                    }
+                }
+                imageFiles = Directory.GetFiles(path)
+                        .Where(file => file.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                                       file.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                                       file.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                                       file.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
+                                       file.EndsWith(".gif", StringComparison.OrdinalIgnoreCase))
+                        .ToArray();
+                be_opened = true;
+                foreach (var file in imageFiles)
+                {
+                  
+                    listBox1.Items.Add(Path.GetFileName(file));
+                }
+            }
+
+            /*   
+                pictureBox1.Image=System.Drawing.Image.FromFile(Path);
+               int imgWidth = pictureBox1.Image.Width;
+               int imgHeight = pictureBox1.Image.Height;
+               int boxWidth = pictureBox1.Width;
+               int boxHeight = pictureBox1.Height;
+
+               // Calculate the aspect ratios of the image and the PictureBox
+               float imgAspectRatio = (float)imgWidth / imgHeight;
+               float boxAspectRatio = (float)boxWidth / boxHeight;
+
+
+               float scaleFactor;
+               if (imgAspectRatio > boxAspectRatio)
+               {
+
+                   scaleFactor = (float)imgWidth / boxWidth;
+               }
+               else
+               {
+
+                   scaleFactor = (float)imgHeight / boxHeight;
+               }
+               pictureBox1.Width = (int)(imgWidth / scaleFactor);
+               pictureBox1.Height = (int)(imgHeight / scaleFactor);
+
+               // Set the image size mode to stretch the image properly
+               pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            */
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width= (int)(pictureBox1.Width*1.1);
+            pictureBox1.Height = (int)(pictureBox1.Height * 1.1);
+            int formWidth = this.ClientSize.Width;
+            int formHeight = this.ClientSize.Height;
+            int newX = (formWidth - pictureBox1.Width) / 2;
+            int newY = (formHeight - pictureBox1.Height) / 2;
+
+            // Set the new location of the PictureBox
+           // pictureBox1.Location = new Point(newX, newY);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width = (int)(pictureBox1.Width * 0.9);
+            pictureBox1.Height = (int)(pictureBox1.Height * 0.9);
+            int formWidth = this.ClientSize.Width;
+            int formHeight = this.ClientSize.Height;
+            int newX = (formWidth - pictureBox1.Width) / 2 ;
+            int newY = (formHeight - pictureBox1.Height) / 2;
+
+            // Set the new location of the PictureBox
+            //pictureBox1.Location = new Point(newX, newY);
+        }
+
+        private void picture_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private Image LoadResizedImage(string imagePath, int maxWidth, int maxHeight)
+        {
+            using (Image originalImage = Image.FromFile(imagePath))
+            {
+                // Calculate new size while maintaining the aspect ratio
+                double ratioX = (double)maxWidth / originalImage.Width;
+                double ratioY = (double)maxHeight / originalImage.Height;
+                double ratio = Math.Min(ratioX, ratioY);
+
+                int newWidth = (int)(originalImage.Width * ratio);
+                int newHeight = (int)(originalImage.Height * ratio);
+
+                // Create a new bitmap with the new size
+                Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+
+                // Draw the original image into the new bitmap, scaling it down
+                using (Graphics graphics = Graphics.FromImage(resizedImage))
+                {
+                    graphics.DrawImage(originalImage, 0, 0, newWidth, newHeight);
+                }
+
+                return resizedImage;
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                // Get the full file path of the selected image
+                string selectedImagePath = imageFiles[selectedIndex];
+
+                // Load and display the selected image in the PictureBox
+                using (Image img = Image.FromFile(selectedImagePath))
+                {
+                    // Optional: Resize PictureBox to fit the image or adjust the PictureBox's SizeMode
+                    pictureBox1 .Image = LoadResizedImage(selectedImagePath, 800, 600);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // Or other modes like Zoom, AutoSize, etc.
+
+                }
+            }
+
+        }
+    }
+}
